c3be4d82e6dcd4b2cc648535fd5cdb39620d0148
Rishita Sahu <sahurishita123@gmail.com>
2025-04-20 17:12:29 +0530

    new design

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ujjain_travel' title`, () => {
+  it(`should have the 'recruitment-management-system' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ujjain_travel');
+    expect(app.title).toEqual('recruitment-management-system');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ujjain_travel');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, recruitment-management-system');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,ReactiveFormsModule,],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
-  title = 'ujjain_travel';
-
-  sub:number=0;
-  myForm:FormGroup;
-
-  constructor(private http:HttpClient){}
-  ngOnInit(){
-    this.myForm = new FormGroup({
-      name :new FormControl("",Validators.required),
-      email:new FormControl("",[Validators.required,Validators.email]),
-      mobile:new FormControl("",[Validators.required,Validators.minLength(10)]),
-      address:new FormControl("",Validators.required),
-      code: new FormControl("",Validators.required)
-    });
-  }
-
-  GetEnquiry(){
-    this.sub=1;
-    if(this.myForm.valid){
-      this.http.post("http://localhost:3001/",this.myForm.value).subscribe();
-      alert("You will get call for enquiry very soon");
-      this.sub=0;
-      this.myForm.reset();
-    }
-  }
+export class AppComponent {
+  title = 'recruitment-management-system';
 }
